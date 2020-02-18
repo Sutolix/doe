29fba7ad46558c623f09cf99dd2dbc4cfb0b8455
Sutolix <tg884ght@gmail.com>
2020-02-18 14:56:19 -0300

    postgree installation

--- services/server.js
+++ services/server.js
@@ -5,15 +5,57 @@ const server = express()
 //Configura para receber arquivos estáticos (css, imagens e afins)
 server.use(express.static('public'))
 
+//Habilitar body do formulário
+server.use(express.urlencoded({ extended: true }))
+
 //Configura a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true, //para não carregar cache durante a produção
 })
+
+//Lista de doadores: Array
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+
+    {
+        name: "Cleiton Souza",
+        blood: "B+"
+    },
+
+    {
+        name: "Matheus Ferreira",
+        blood: "A+"
+    },
+
+    {
+        name: "Jaqueline Silva",
+        blood: "O-"
+    }
+]
 
 //Configura a apresentação da página
 server.get("/", function(req, res){
-    return res.render("index.html")
+    return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res){
+    //pegar dados do formulário
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    //coloca valores dentro do array
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
 })
 
 //Liga o servidor e permite seu acesso na porta 3000
